bc081280ba51ceff669f9ce865be5128410eed05
Nazifa Karima <karima.2@wright.edu>
2015-11-03 13:15:06 -0500

    https://www.hackerrank.com/challenges/angry-professor

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
-
 public class Solution {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String givenTime = sc.next();
-		String[] token = givenTime.split(":");
-		//String hour = givenTime.substring(0, 1);
-		String a = givenTime.substring(8, 10);
-		System.out.println(a);
-		Integer h = Integer.parseInt(token[0]);
-		if (a.contentEquals("PM")) {
-			if (h != 12)
-			{
-				int tmp = h + 12;
-				token[0] = Integer.toString(tmp);
-			}
-		}else {
-			if (h == 12) {
-				token[0] = "00";
-			}
-		}
-		String last = token[2].substring(0, 2);
-		System.out.println(token[0] + ":" + token[1] + ":" + last);
-	}
+		int loop = sc.nextInt();
 
+		while (loop > 0) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			int beforeClass = 0;
+			for (int i = 0; i < n; i++) {
+				int tmp = sc.nextInt();
+				if (tmp <= 0) {
+					beforeClass++;
+				}
+			}
+			if (beforeClass < k) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+			loop--;
+		}
+	}
 }
