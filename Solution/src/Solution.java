fde8defbac2189e48e2ff544f409dcd29b400672
Nazifa Karima <karima.2@wright.edu>
2015-11-03 15:55:23 -0500

    Timeout for several testcases:https://www.hackerrank.com/challenges/sherlock-and-the-beast/

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -4,24 +4,71 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int loop = sc.nextInt();
-
-		while (loop > 0) {
-			int n = sc.nextInt();
-			int k = sc.nextInt();
-			int beforeClass = 0;
-			for (int i = 0; i < n; i++) {
-				int tmp = sc.nextInt();
-				if (tmp <= 0) {
-					beforeClass++;
+		int t = Integer.parseInt(sc.nextLine());
+		int[] input = new int[t]; 
+		for (int i = 0; i < t; i++) {
+			input[i] = Integer.parseInt(sc.nextLine());
+		}
+		for (int j = 0; j < input.length; j++) {
+			int n = input[j];
+			boolean resultFound = false;
+			if (isDivisibleByThree(n)) {
+				resultFound = true;
+				createAndPrintresult(n, 0);
+				
+			} else if (!resultFound && !isDivisibleByThree(n)) {
+				int divisor = n / 3;
+				while (divisor > 0) {
+					int noOfThree = n - (divisor * 3);
+					if (isDivisibleByFive(noOfThree)) {
+						resultFound = true;
+						createAndPrintresult(n, noOfThree);
+					}
+					divisor--;
 				}
 			}
-			if (beforeClass < k) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
+			if (!resultFound) {
+				if (isDivisibleByFive(n)) {
+					resultFound = true;
+createAndPrintresult(n, n);
+				}
 			}
-			loop--;
+
+			if (!resultFound) {
+				System.out.println("-1");
+			}
 		}
+	}
+
+	private static void createAndPrintresult(int n, int noOfThree) {
+		int[] result = new int[n];
+		for (int i = 0; i < result.length; i++) {
+			if (i < (result.length - noOfThree)) {
+				result[i] = 5;
+			} else {
+				result[i] = 3;
+			}
+		}
+		printResult(result);
+	}
+
+	private static void printResult(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i]);
+		}System.out.println();
+	}
+
+	private static boolean isDivisibleByThree(int n) {
+		if (n / 3 > 0 && n % 3 == 0)
+			return true;
+		else
+			return false;
+	}
+
+	private static boolean isDivisibleByFive(int n) {
+		if (n / 5 > 0 && n % 5 == 0)
+			return true;
+		else
+			return false;
 	}
 }
