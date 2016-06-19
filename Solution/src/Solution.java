c3461dc68056d93e164b15114fc560ceaa8df88a
Nazifa Karima <karima.2@wright.edu>
2016-06-19 19:07:48 -0400

    https://www.hackerrank.com/challenges/find-digits?h_r=next-challenge&h_v=zen

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,37 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
-	public static void insertIntoSorted(int[] ar) {
-		int pivot = ar[ar.length - 1];
-		int i = ar.length - 2;
-		while (i >= 0 && pivot < ar[i]) {
-			ar[i+1] = ar[i];	
-			printArray(ar);
-			i--;
-		}
-		if(i<0)
-			ar[0] = pivot;
-		else
-			ar[i+1]=pivot;
-		printArray(ar);
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            String numString = String.valueOf(n);
+            char[] digits = numString.toCharArray();
+            int count = 0;
+            for (int i = 0; i < digits.length; i++) {
+           	int x = Character.getNumericValue(digits[i]);
+            	if(x != 0 && n%x == 0){
+            		count++;
+            	}
+			}
+            System.out.println(count);
+        }
 
-	/* Tail starts here */
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int s = in.nextInt();
-		int[] ar = new int[s];
-		for (int i = 0; i < s; i++) {
-			ar[i] = in.nextInt();
-		}
-		insertIntoSorted(ar);
-	}
-
-	private static void printArray(int[] ar) {
-		for (int n : ar) {
-			System.out.print(n + " ");
-		}
-		System.out.println("");
-	}
+    }
 }
