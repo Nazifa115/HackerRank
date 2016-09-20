ae3c42e3d642ec942e8d5a38be5e354e816db709
Nazifa Karima <karima.2@wright.edu>
2016-09-20 19:53:18 -0400

    Grid Illumination

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -10,44 +10,59 @@ import java.util.Set;
 class Solution {
 	public static void main(String[] argh) {
 		Scanner in = new Scanner(System.in);
-		//int n = in.nextInt();
-		
-		while (in.hasNext()) {
-			HashMap<Integer, Integer> numberOfDigit = new HashMap<Integer, Integer>();
-			String curString = in.next();
-			Integer current = Integer.parseInt(curString);
-			while (current > 0) {
-				Integer i = current % 10;
-				if (numberOfDigit.containsKey(i)) {
-					Integer value = numberOfDigit.get(i);
-					value++;
-					numberOfDigit.put(i, value);
-				} else {
-					numberOfDigit.put(i, 1);
+					
+	in.close();
+	}
+	class Board{
+		int dim;
+		Cell[][] board;
+		public Board(int N, int[][] lamps) {
+			this.dim = N;
+			board =  new Cell[dim][dim];
+			for (int i = 0; i < dim; i++) {
+				for (int j = 0; j < dim; j++) {
+					Cell c = new Cell(i, j, false);
 				}
-				current = current / 10;
 			}
-			StringBuilder builder = new StringBuilder();		
-				Set s = numberOfDigit.entrySet();
-				Iterator iter = s.iterator();
-				while (iter.hasNext()) {
-					Map.Entry mapping = (Map.Entry)iter.next();
-					String key = mapping.getKey().toString();
-					String value = mapping.getValue().toString();
-					builder.append(value);
-					builder.append(key);
+			for (int p = 0; p < lamps.length; p++) {
+				int lamp_x = lamps[p][0]-1;
+				int lamp_y = lamps[p][1]-1;	
+				for (int i = 0; i < dim; i++) {
+					for (int j = 0; j < dim; j++) {
+						Cell c = board[i][j];
+						if(c.x == lamp_x) c.illuminated = true;
+						else if(c.y == lamp_y) c.illuminated = true;
+						else {
+							int dia_x = Math.abs(c.x-lamp_x);
+							int dia_y = Math.abs(c.y-lamp_y);
+							if(dia_x == dia_y) c.illuminated = true;						
+						}
+					}
 				}
-				   
-				ArrayList<String> resultStrings = new ArrayList<String>();
-				String[] result = new String[resultStrings.size()];
-				for (int i = 0; i < resultStrings.size(); i++) {
-			        result[i] = resultStrings.get(i).toString();
-			    }
-		System.out.println(builder.toString());
+				
+			}
 		}
-		in.close();
+		
+		class Cell{
+			int x, y;
+			boolean illuminated;
+			public Cell(int coord_x, int coord_y, boolean lit) {
+				this.x = coord_x;
+				this.y = coord_y;
+				this.illuminated = lit;
+			}
+		}
+		
+		
 	}
 
+	 String[] checkIllumination(int N, int[][] lamps, int[][] queries) {
+        String[] results = null;
+        Board board = new Board(0, lamps);
+        int i = queries[0].length;
+        return results;
+    }
+	
 	public static String interleave(String str1, String str2) {
 		int maxLen = str1.length() < str2.length() ? str2.length() : str1
 				.length();
