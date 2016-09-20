21592883e5eb8a976c01c37b15749a7c00504615
Nazifa Karima <karima.2@wright.edu>
2016-09-20 18:39:55 -0400

    Dropbox Coding test

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,44 +1,69 @@
 //Complete this code or write your own from scratch
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
 
-class Solution{
-    public static void main(String []argh){
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        HashMap<String, Integer> contacts = new HashMap<String, Integer>();
-        for(int i = 0; i < n; i++){
-            String name = in.next();
-            int phone = in.nextInt();
-            contacts.put(name, phone);
-        }
-        while(in.hasNext()){
-            String s = in.next();
-            Integer value = contacts.get(s);
-            if(value != null)
-            	System.out.println(s+"="+value);
-            else
-            	System.out.println("Not found");
-        }
-        in.close();
-    }
-    
-    public static String interleave(String str1, String str2) {
-        int maxLen = str1.length() < str2.length()? str2.length():str1.length();
-        ArrayList<Character> resultList = new ArrayList<Character>();
-        char[] str1ToCharArray = str1.toCharArray();
-        char[] str2ToCharArray = str2.toCharArray();
-        for(int i=0; i< maxLen; i++){
-        	if(i < str1.length())
-        		resultList.add((Character)str1ToCharArray[i]);
-        	if(i < str2.length())
-        		resultList.add((Character)str2ToCharArray[i]);
-        }
-        StringBuilder builder = new StringBuilder(resultList.size());
-        for(Character ch: resultList)
-        {
-            builder.append(ch);
-        }
+class Solution {
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		//int n = in.nextInt();
+		
+		while (in.hasNext()) {
+			HashMap<Integer, Integer> numberOfDigit = new HashMap<Integer, Integer>();
+			String curString = in.next();
+			Integer current = Integer.parseInt(curString);
+			while (current > 0) {
+				Integer i = current % 10;
+				if (numberOfDigit.containsKey(i)) {
+					Integer value = numberOfDigit.get(i);
+					value++;
+					numberOfDigit.put(i, value);
+				} else {
+					numberOfDigit.put(i, 1);
+				}
+				current = current / 10;
+			}
+			StringBuilder builder = new StringBuilder();		
+				Set s = numberOfDigit.entrySet();
+				Iterator iter = s.iterator();
+				while (iter.hasNext()) {
+					Map.Entry mapping = (Map.Entry)iter.next();
+					String key = mapping.getKey().toString();
+					String value = mapping.getValue().toString();
+					builder.append(value);
+					builder.append(key);
+				}
+				   
+				ArrayList<String> resultStrings = new ArrayList<String>();
+				String[] result = new String[resultStrings.size()];
+				for (int i = 0; i < resultStrings.size(); i++) {
+			        result[i] = resultStrings.get(i).toString();
+			    }
+		System.out.println(builder.toString());
+		}
+		in.close();
+	}
+
+	public static String interleave(String str1, String str2) {
+		int maxLen = str1.length() < str2.length() ? str2.length() : str1
+				.length();
+		ArrayList<Character> resultList = new ArrayList<Character>();
+		char[] str1ToCharArray = str1.toCharArray();
+		char[] str2ToCharArray = str2.toCharArray();
+		for (int i = 0; i < maxLen; i++) {
+			if (i < str1.length())
+				resultList.add((Character) str1ToCharArray[i]);
+			if (i < str2.length())
+				resultList.add((Character) str2ToCharArray[i]);
+		}
+		StringBuilder builder = new StringBuilder(resultList.size());
+		for (Character ch : resultList) {
+			builder.append(ch);
+		}
 		return builder.toString();
-    }
+	}
 }
