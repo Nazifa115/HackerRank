485e21f3344dea343186663e9d7a4e8cbd56686a
nazifakarima <nazifakarima@gmail.com>
2016-11-29 19:25:39 -0500

    https://www.hackerrank.com/challenges/30-recursion

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,28 +1,18 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		/*
-		 * Enter your code here. Read input from STDIN. Print output to STDOUT.
-		 * Your class should be named Solution.
-		 */
-		Scanner sc = new Scanner(System.in);
-		Integer n = sc.nextInt();
-		Integer k = sc.nextInt();
-		ArrayList<Integer> input = new ArrayList<Integer>();
-		for (Integer i = 0; i < n; i++) {
-			input.add(sc.nextInt());
-		}
-		Collections.sort(input);
-		Collections.reverse(input);
-
-		Integer sum = 0;
-		for (Integer i = 0; i < n; i++) {
-			sum+= (i/k + 1) * input.get(i);
-		}
-		System.out.println(sum);
-	}
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */ 
+        Scanner sc = new Scanner(System.in);
+        Integer n = sc.nextInt();
+        Long fac = factorial(n);
+        System.out.println(fac);
+    }
+    public static Long factorial(int n){
+        if(n == 1)
+            return (long) 1;
+        else return (n*factorial(n-1));
+    }
 }
