cec6ddc775511fa12685209ff3d177a01aabca57
nazifakarima <nazifakarima@gmail.com>
2017-06-21 01:42:30 -0400

    Merged properly

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,17 +1,29 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Solution {
-
-    public static void main(String[] args) {
+    public static void main(String args[] ) throws Exception {
         Scanner in = new Scanner(System.in);
-        int[] h = new int[26];
-        for(int h_i=0; h_i < 26; h_i++){
-            h[h_i] = in.nextInt();
+        int n = in.nextInt();
+        int sum = 0;
+        ArrayList<Integer> runningList = new ArrayList<>();
+        for(int i=0; i<= n; i++){
+            Integer element = in.nextInt();
+            runningList.add(element);
+            Collections.sort(runningList);
+            double median;
+            if(i==0){
+            	median = runningList.get(0);
+            }
+            else if(i%2 != 0){//even
+            	int index = i/2;
+                median = (double)(runningList.get(index) + runningList.get(index+1))/2.00;
+            }
+            else{//odd
+            	median = (double)runningList.get(i/2);
+            }
+            System.out.println(median);
         }
-        String word = in.next();
     }
 }
