ec9af1d05657565b7954538e5c0ac4aae788e9d8
nazifakarima <nazifakarima@gmail.com>
2017-06-21 22:38:43 -0400

    stack class done, although Java has it's own stack class

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String args[] ) throws Exception {
+    	
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int sum = 0;
