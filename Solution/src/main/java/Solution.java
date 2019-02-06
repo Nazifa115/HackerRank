acbace501c7af4977d892fb88a1bc7a4a51a82ba
Nazifa <nazifa.karima@cengage.com>
2019-02-06 15:05:11 -0500

    Given a list of co-ordinates return the number of nearest restaurant requested by the user

--- Solution/src/main/java/Solution.java
+++ Solution/src/main/java/Solution.java
@@ -1,33 +1,28 @@
-// IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
-// SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
-// DEFINE ANY CLASS AND METHOD NEEDED
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 
-public class Solution
-{
-	//Given a list of forward routes and return routes, find the optimal route/s which is the closest (or equal) but not greater than the maxTravelDist.
-	public List<List<Integer>> findMaxTravelDistance(List<List<Integer>> forwardRouteList, List<List<Integer>> returnRouteList, Integer maxTravelDist)
-	{
-		Map<Integer, List<List<Integer>>> optimalTravelDistance = newHashMap();
-		Integer maxCombinedDistFound = -1;
-		for (List<Integer> fwdDistanceIdPair:forwardRouteList) {
-			for (List<Integer> retDistanceIDPair:returnRouteList) {
-				Integer combinedDistance = fwdDistanceIdPair.get(1) + retDistanceIDPair.get(1);
-				if(combinedDistance <= maxTravelDist && combinedDistance >=maxCombinedDistFound) {
-					maxCombinedDistFound = combinedDistance;
-					List<Integer> pairToAdd = newArrayList(fwdDistanceIdPair.get(0), retDistanceIDPair.get(0));
-					List<List<Integer>> existingPairs = optimalTravelDistance.containsKey(combinedDistance)? optimalTravelDistance.get(combinedDistance):newArrayList();
-					existingPairs.add(pairToAdd);
-					optimalTravelDistance.put(combinedDistance, existingPairs);
-				}
-			}
-		}
-		List<Integer> sortedOptimalDistance = newArrayList(optimalTravelDistance.keySet());
-		Collections.sort(sortedOptimalDistance, Collections.reverseOrder());
-		if(optimalTravelDistance.size()>0) return optimalTravelDistance.get(sortedOptimalDistance.get(0));
-		else return newArrayList();
-	}
+public class Solution {
+    //Given a list of co-ordinates return the number of nearest restaurant requested by the user
+    List<List<Integer>> nearestVegetarianRestaurant(int totalRestaurants,
+                                                    List<List<Integer>> allLocations,
+                                                    int numRestaurants) {
+        Map<Double, List<Integer>> distances = newHashMap();
+
+        for (List<Integer> location : allLocations) {
+            Double distance = Math.sqrt((location.get(0) * location.get(0)) + (location.get(1) * location.get(1)));
+            distances.put(distance, location);
+        }
+        List<Double> sortedDistance = newArrayList(distances.keySet());
+        Collections.sort(sortedDistance);
+        List<List<Integer>> results = newArrayList();
+        for (int i = 0; i < numRestaurants; i++) {
+            results.add(distances.get(sortedDistance.get(i)));
+        }
+        return results;
+    }
 }
